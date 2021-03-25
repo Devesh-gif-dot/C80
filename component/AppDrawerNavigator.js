3bbd37924178dbca00ccddb05defb9936de880f5
Devesh-gif-dot <70578513+Devesh-gif-dot@users.noreply.github.com>
2021-03-25 17:00:46 +0530

    Add files via upload

---
+++ component/AppDrawerNavigator.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react';
+import {createDrawerNavigator} from 'react-navigation-drawer';
+import TabNavigator from './AppTabNavigator'
+import CustomSideBarMenu from './CustomSideBarMenu'
+
+export const AppDrawerNavigator = createDrawerNavigator({
+    Home:{screen:TabNavigator}
+},
+{
+    contentComponent:CustomSideBarMenu
+},
+{
+    initialRouteName:'Home',
+})
